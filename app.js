dd77a596d7081342610ed1a73711730d08664f26
nbirhanu <nbirhanu80@gmail.com>
2024-01-25 20:31:19 +0300

    post documents

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 //READ ALL TENDERS
 
 const tenders = JSON.parse(fs.readFileSync(`${__dirname}/data/allTenders.json`))
@@ -15,6 +17,22 @@ app.get('/api/v1/tenders', (req, res) => {
 		data: {
 			tenders,
 		},
+	})
+})
+
+//CREATE TENDERS
+
+app.post('/api/v1/tenders', (req, res) => {
+	const newTender = Object.assign(req.body)
+	console.log(newTender)
+	tenders.push(newTender)
+	fs.writeFile(`${__dirname}/data/allTenders.json`, JSON.stringify(tenders), err => {
+		res.status(200).json({
+			status: 'success',
+			data: {
+				tenders: newTender,
+			},
+		})
 	})
 })
 
