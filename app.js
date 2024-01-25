22598034f50a65803c738311bfc419f054336af0
nbirhanu <nbirhanu80@gmail.com>
2024-01-25 19:10:50 +0300

    build the link of get all tenders

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const app = express()
 
 //READ ALL TENDERS
 
-const tends = JSON.parse(fs.readFileSync(`${__dirname}/data/allTenders.json`))
+const tenders = JSON.parse(fs.readFileSync(`${__dirname}/data/allTenders.json`))
+
+//GET TENDERS
+app.get('/api/v1/tenders', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		result: tenders.length,
+		data: {
+			tenders,
+		},
+	})
+})
 
 //MIDDLEWARE
 app.use(express.json())
 
+//PORT
 const port = 7000
 app.listen(port, () => {
 	console.log(`app running on port ${port}`)
