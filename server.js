6ecbc525c2ded977da3010632e578e0823fde3ca
nbirhanu <nbirhanu80@gmail.com>
2024-01-30 23:25:45 +0300

    set Connection events

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE_STRING.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB);
+
+// Connection events
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+// Close the Mongoose connection when the Node process is terminated
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
 const port = 7000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
