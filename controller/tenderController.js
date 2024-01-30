0202dc28002b5e44f6c3d909d280e3ed5e788ec0
nbirhanu <nbirhanu80@gmail.com>
2024-01-31 00:22:59 +0300

    create new tender

--- controller/tenderController.js
+++ controller/tenderController.js
@@ -1,80 +1,84 @@
-const fs = require('fs')
-
-const tenders = JSON.parse(fs.readFileSync(`${__dirname}/../data/allTenders.json`))
-
-//GET TENDERS
-exports.getAllTenders = (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		result: tenders.length,
-		data: {
-			tenders,
-		},
-	})
-}
+const fs = require('fs');
+const Tender = require('./../models/tenderModel');
 
 //CREATE TENDER
-exports.createTender = (req, res) => {
-	const newTender = Object.assign(req.body)
-	console.log(newTender)
-	tenders.push(newTender)
-	fs.writeFile(`${__dirname}/data/allTenders.json`, JSON.stringify(tenders), err => {
-		res.status(200).json({
-			status: 'success',
-			data: {
-				tenders: newTender,
-			},
-		})
-	})
-}
+exports.createTender = async (req, res) => {
+  try {
+    const newTender = await Tender.create();
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tenders: newTender,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+};
+
+//GET TENDERS
+exports.getAllTenders = async (req, res) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+      result: tenders.length,
+      data: {
+        tenders,
+      },
+    });
+  } catch (err) {}
+};
 
 //get tender by id
 exports.getTender = (req, res) => {
-	const id = +req.params.id
-	const tender = tenders.find(el => el.id === id)
-	if (!tender) {
-		return res.status(204).json({
-			status: 'fail',
-			message: 'invalid id',
-		})
-	}
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tender,
-		},
-	})
-}
+  const id = +req.params.id;
+  const tender = tenders.find((el) => el.id === id);
+  if (!tender) {
+    return res.status(204).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tender,
+    },
+  });
+};
 
 // UPDATE TENDER
 exports.updateTenders = (req, res) => {
-	const id = +req.params.id
-	if (id > tenders.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'invalid id',
-		})
-	}
+  const id = +req.params.id;
+  if (id > tenders.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
 
-	res.status(200).json({
-		status: 'success',
-		data: '<update tender here...>',
-	})
-}
+  res.status(200).json({
+    status: 'success',
+    data: '<update tender here...>',
+  });
+};
 
 //DELETE TENDER
 exports.deleteTender = (req, res) => {
-	const id = +req.params.id
-	if (id > tenders.length) {
-		return res.status(404).json({
-			status: 'fail',
-			message: 'invalid id',
-		})
-	}
-	res.status(204).json({
-		status: 'success',
-		data: {
-			tender: null,
-		},
-	})
-}
+  const id = +req.params.id;
+  if (id > tenders.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tender: null,
+    },
+  });
+};
